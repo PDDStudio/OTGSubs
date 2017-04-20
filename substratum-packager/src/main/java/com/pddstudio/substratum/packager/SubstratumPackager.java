eb7dbf51c8ad21c6fed6de912286702746f55f67
Patrick J <patrick.pddstudio@googlemail.com>
2017-04-20 14:53:58 +0200

    Added skeleton class for substratum packager

---
+++ substratum-packager/src/main/java/com/pddstudio/substratum/packager/SubstratumPackager.java
@@ -0,0 +1,17 @@
+package com.pddstudio.substratum.packager;
+
+/**
+ * Created by pddstudio on 20/04/2017.
+ */
+
+public class SubstratumPackager {
+
+	private SubstratumPackager(Builder builder) {
+
+	}
+
+	public static final class Builder {
+
+	}
+
+}
