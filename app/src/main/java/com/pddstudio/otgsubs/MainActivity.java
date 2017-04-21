9b4e8a7aafa560184af7756ff6c6ff47959121dd
Patrick J <patrick.pddstudio@googlemail.com>
2017-04-21 11:39:09 +0200

    Dummy implementation for initial testing

--- app/src/main/java/com/pddstudio/otgsubs/MainActivity.java
+++ app/src/main/java/com/pddstudio/otgsubs/MainActivity.java
@@ -3,11 +3,14 @@ package com.pddstudio.otgsubs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pddstudio.substratum.packager.SubstratumPackager;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		new SubstratumPackager.Builder(this).build().doWork();
 	}
 }
