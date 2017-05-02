6cff92bd1dee435c5e0274e2495fd042f6cc6178
Patrick J <patrick.pddstudio@googlemail.com>
2017-05-02 15:22:28 +0200

    Added HierarchyViewerFactory to possibly invoke each Layout creation via callback

---
+++ substratum-debugger/src/main/java/com/pddstudio/substratum/debugger/factory/HierarchyViewerFactory.java
@@ -0,0 +1,45 @@
+package com.pddstudio.substratum.debugger.factory;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * Created by pddstudio on 02/05/2017.
+ */
+
+public class HierarchyViewerFactory implements LayoutInflater.Factory2 {
+
+	private final LayoutInflater.Factory2 baseFactory;
+	private InflationCallback callback;
+
+	public HierarchyViewerFactory(@NonNull LayoutInflater.Factory2 factory) {
+		this.baseFactory = factory;
+	}
+
+	private void onInterceptLayout(View parent, String name, Context context, AttributeSet attrs) {
+		if(callback != null) {
+			callback.onLayoutInflated(parent, name, context, attrs);
+		}
+	}
+
+	@Override
+	public View onCreateView(String name, Context context, AttributeSet attrs) {
+		onInterceptLayout(null, name, context, attrs);
+		return baseFactory.onCreateView(name, context, attrs);
+	}
+
+	@Override
+	public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+		onInterceptLayout(parent, name, context, attrs);
+		return baseFactory.onCreateView(parent, name, context, attrs);
+	}
+
+	public interface InflationCallback {
+		void onLayoutInflated(@Nullable View parent, String name, Context context, AttributeSet attrs);
+	}
+
+}
